d12b86e97e052846be0dd8052eda433c861eb42e
Daywison <daywison.leal@gmail.com>
2020-05-30 15:12:23 -0300

    Resolvido o desafio 09 do Bootcamp GoStack

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -1,4 +1,4 @@
-import { inject, injectable } from 'tsyringe';
+import { inject, injectable, container } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
@@ -20,13 +20,54 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found!');
+    }
+
+    const productsPrice = await this.productsRepository.findAllById(
+      products.map(product => ({ id: product.id })),
+    );
+
+    if (products.length !== productsPrice.length) {
+      throw new AppError('Product not found!');
+    }
+
+    products.forEach(product => {
+      productsPrice.forEach(productPrice => {
+        if (
+          productPrice.id === product.id &&
+          productPrice.quantity < product.quantity
+        ) {
+          throw new AppError('Quantity Insufficient');
+        }
+      });
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: products.map(product => ({
+        product_id: product.id,
+        price: productsPrice.find(({ id }) => id === product.id)?.price || 0,
+        quantity: product.quantity,
+      })),
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
